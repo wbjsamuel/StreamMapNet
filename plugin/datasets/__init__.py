794f641226e8bf7e9a68f35622660b91ca0e1dcb
wbjsamuel <307284908@qq.com>
2023-09-13 09:50:57 +0000

    renew init

--- plugin/datasets/__init__.py
+++ plugin/datasets/__init__.py
@@ -1,3 +1,5 @@
 from .pipelines import *
 from .argo_dataset import AV2Dataset
 from .nusc_dataset import NuscDataset
+from .argo_unimapping_dataset import AV2_UniMapping_Dataset
+from .openlane_v2_av2_dataset import OpenLaneV2_Av2_Dataset
